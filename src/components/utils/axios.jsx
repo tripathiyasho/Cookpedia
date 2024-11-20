c37a925febd344bb8ab860d7e7df9c6f87c0d692
tripathiyasho <yasho130802@gmail.com>
2024-11-20 13:06:12 +0530

    update

--- src/components/utils/axios.jsx
+++ src/components/utils/axios.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const tastyAPI = axios.create({
   baseURL: "https://tasty.p.rapidapi.com/", // Set the base URL
   headers: {
-    "x-rapidapi-key": "13e0d6bc52mshf9f21caa0619c2bp1d64b9jsnc74dd00732c3" , // Your API key
+    "x-rapidapi-key": import.meta.env.VITE_APP_RAPIDAPI_KEY , // Your API key
     "x-rapidapi-host": "tasty.p.rapidapi.com", // The host for the API
   },
 });
